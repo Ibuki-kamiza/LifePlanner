278e7b130ff033681511c0fdd1415d4c28ad0f84
ibuki-kamiza <shangjingyichui@gmail.com>
2026-04-27 09:34:56 +0900

    Initial Commit

---
+++ LifePlannerTests/LifePlannerTests.swift
@@ -0,0 +1,19 @@
+//
+//  LifePlannerTests.swift
+//  LifePlannerTests
+//
+//  Created by pc on 2026/04/27.
+//
+
+import Testing
+@testable import LifePlanner
+
+struct LifePlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
